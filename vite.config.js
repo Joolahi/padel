498a66a6b4f3a9908be1efb82bdb7468d1ec2dc9
Joolahi <joonaslahti1998@gmail.com>
2024-06-17 18:15:59 +0300

    mods

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,15 @@ const myEnv = dotenv.config();
 dotenvExpand.expand(myEnv);
 
 export default defineConfig({
+  base: "/padel/",
   define: {
-    base: "/padel/",
-    'process.env': process.env
+    'process.env': {
+      VITE_API_KEY: process.env.VITE_API_KEY,
+      VITE_AUTH_DOMAIN: process.env.VITE_AUTH_DOMAIN,
+      VITE_PROJECT_ID: process.env.VITE_PROJECT_ID,
+      VITE_STORAGE_BUCKET: process.env.VITE_STORAGE_BUCKET,
+      VITE_MESSAGING_SENDER_ID: process.env.VITE_MESSAGING_SENDER_ID,
+      VITE_APP_ID: process.env.VITE_APP_ID,
+    }
   }
 });
